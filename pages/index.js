13eb47ed4592cb7a995dae651a1443c0f890c531
Daniel Brazil <danielbrazil@hotmail.com>
2021-02-15 14:09:29 -0500

    Aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,25 @@
+import React from 'react';
 import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizBackground';
-
-
+import QuizLogo from '../src/components/QuizLogo';
 
 // const Title = styled.h1`
 //   font-size: 50px;
 //   color: ${({ theme }) => theme.colors.primary};
 // `
-//qualquer funcao iniciada com letra maiuscula definem um componente no REACT
-//a assinatura da funcao pode utilizar qualquer nome de variavel, mas por padrao usar props
+// qualquer funcao iniciada com letra maiuscula definem um componente no REACT
+// a assinatura da funcao pode utilizar qualquer nome de variavel, mas por padrao usar props
 // function Title(props) {
-//     return ( 
-//         <h1 > 
-//             { props.children } 
-//         </h1> 
+//     return (
+//         <h1 >
+//             { props.children }
+//         </h1>
 //     )
 // }
 
@@ -33,7 +35,7 @@ export const QuizContainer = styled.div`
   }
 `;
 
-//sintaxe: javascript 
+// sintaxe: javascript
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
 //   flex: 1;
@@ -42,27 +44,55 @@ export const QuizContainer = styled.div`
 // `;
 
 export default function Home() {
-    return (
-      //aqui ja eh react atuando por isso nao tem $
-      <QuizBackground backgroundImage={db.bg}>
-        <QuizContainer>
-          <Widget>
-            <Widget.Header>
-              <h1>Quiz sobre o Canadá</h1>
-            </Widget.Header>
-            <Widget.Content>
-              <p align="justify">O Canadá é um país que ocupa grande parte da América do Norte e se estende desde o oceano Atlântico, a leste, até o oceano Pacífico, a oeste. Ao norte o país é limitado pelo oceano Ártico. É o segundo maior país do mundo em área total, superado apenas pela Rússia, e a sua fronteira comum com os Estados Unidos, no sul e no noroeste, é a mais longa fronteira terrestre do mundo. </p>
-            </Widget.Content>
-          </Widget>
-          <Widget>
-            <Widget.Content>
-              <h1>Quizes da Galera</h1>
-              <p>...</p>
-            </Widget.Content>
-          </Widget>
-          <Footer />
-        </QuizContainer>
-        <GitHubCorner projectUrl="https://github.com/omariosouto" />
-      </QuizBackground>
-    )
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
+  return (
+  // aqui ja eh react atuando por isso nao tem $, o onSubmit no form eh react
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Alura Quiz - Modelo Base</title>
+      </Head>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>Quiz sobre o Canadá</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p align="justify">O Canadá é um país que ocupa grande parte da América do Norte e se estende desde o oceano Atlântico, a leste, até o oceano Pacífico, a oeste. Ao norte o país é limitado pelo oceano Ártico. É o segundo maior país do mundo em área total, superado apenas pela Rússia, e a sua fronteira comum com os Estados Unidos, no sul e no noroeste, é a mais longa fronteira terrestre do mundo. </p>
+            <form onSubmit={function (event) {
+              event.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              // eslint-disable-next-line no-console
+              console.log('fazendo uma submissao por meio do react');
+            }}
+            >
+              <input
+                onChange={function (event) {
+                  // state
+                  // name = event.target.value;
+                  setName(event.target.value);
+                }}
+                placeholder="Preencha seu nome..."
+
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+            <p>...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+    </QuizBackground>
+  );
 }
